4fcd30b25d2446dfa03df6ac281d10b9fc12ceb8
ivan <ivansamovarov777@gmail.com>
2024-06-06 18:29:25 +0300

    lesson 4

---
+++ lesson 4/app.js
@@ -0,0 +1,5 @@
+const array_counter = function (array) {
+    console.log("В массиве " + array.length + " элементов");
+};
+
+array_counter([1, 2, 3, 4, 5]);
---
+++ lesson 4/app.py
@@ -0,0 +1,5 @@
+def array_counter(array):
+    print("В массиве ", len(array), " элементов")
+
+
+array_counter([1, 2, 3, 4, 5])
