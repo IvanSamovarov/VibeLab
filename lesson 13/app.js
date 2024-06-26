a2b5f35b75d77a2d10f03519abf5d9ede31ca15d
ivan <ivansamovarov777@gmail.com>
2024-06-26 14:26:13 +0300

    lesson 13

---
+++ lesson 13/app.js
@@ -0,0 +1,5 @@
+import { calculate } from "./module1.js";
+import { Hi, name } from './module2.js'
+
+console.log(calculate(2, 2));
+Hi(name);
---
+++ lesson 13/module2.js
@@ -0,0 +1,7 @@
+const name = "Ivan";
+
+const Hello = (name) => {
+    console.log("Hello,", name);
+}
+
+export {name, Hello as Hi}
