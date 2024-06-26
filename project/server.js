f5953fad3c7d58289f9cca0768863af119d8f3b4
ivan <ivansamovarov777@gmail.com>
2024-06-26 21:04:01 +0300

    lesson 16

---
+++ project/server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+    res.send('<h1>Главная страница</h1>');
+});
+
+app.get('/page1', (req, res) => {
+    res.send('<h1>Страница 1</h1>');
+});
+
+app.get('/page2', (req, res) => {
+    res.send('<h1>Страница 2</h1>');
+});
+app.listen(port, () => {
+    console.log("Сервер запущен");
+});
